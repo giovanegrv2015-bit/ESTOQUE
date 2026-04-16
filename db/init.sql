c6d7c9ca3679b44bb5bc4d792fb25c48440fb0af
GIOVANE PEREIRA DOS SANTOS <023.957019@universidadesenaicimatec.edu.br>
2026-04-15 21:39:13 -0300

    tela de cadastrohtml, css

--- db/init.sql
+++ db/init.sql
@@ -28,3 +28,17 @@ telefone, funcao, CEP, endereco, numero, complemento, bairro, cidade, estado)
 VALUES ('admin', ('1234'), 'Giovane', 'Pereira dos Santos', '123456', '123.456.789-00', '2002-02-18',
  'giovanegrv2015@gmail.com', '(71) 99999-9999', 'Tec. DS', '40050-100', 'Rua Amparo do Tororó',
  29, 'Casa Verde', 'Tororó', 'Salvador', 'Bahia');
+
+create table produtos (
+        id int auto_icrement primary key,
+        codigo_barras varchar(100) not null,
+        nome_produto varchar(100) not null,
+        fabricante varchar(100),
+        marca varchar(100),
+        data_fabricacao date,
+        data_vencimento date,
+        quantidade BIGINT,
+        valor decimal(10,2),
+        total decimal(10,2)
+);
+
---
+++ src/main/java/controller/CadastroProdutosController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.CadastroProdutoModel;
+import dao.CadastroProdutosDAO;
+
+@WebServlet("/cadastroproduto")
+public class CadastroProdutosController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        CadastroProdutoModel produto = new CadastroProdutoModel();
+
+        produto.setCodigoBarras(request.getParameter("codigoBarras"));
+        produto.setNomeProduto(request.getParameter("nomeProdutos"));
+        produto.setFabricante(request.getParameter("fabricante"));
+        produto.setMarca(request.getParameter("marca"));
+        produto.setDataFabricacao(request.getParameter("dataFabricaco"));
+        produto.setDataVencimento(request.getParameter("dataVencimento"));
+        produto.setQuantidade(Long.parseLong(request.getParameter("quantidade")));
+        produto.setValor(request.getParameter("valor"));
+        produto.setTotal(request.getParameter("total"));
+
+        CadastroProdutosDAO dao = new CadastroProdutosDAO();
+
+        if (dao.salvar(produto)) {
+            response.sendRedirect("pages/dashboard.html");
+        } else {
+            response.sendRedirect("pages/cadastroProduto.html");
+        }
+    }
+}
